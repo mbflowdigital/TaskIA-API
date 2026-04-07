f01aacad01d16a7b5f270b4ce77e4fc736fdbb26
joaoabreu22 <abreunegocio@gmail.com>
2026-04-07 17:57:48 -0300

    Ajustes bug no onboarding

---
+++ Infrastructure/Data/Migrations/20260407204336_AddIsOnboardingCompleted.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsOnboardingCompleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PositionId",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOnboardingCompleted",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            // Usuários que já possuem empresa vinculada consideram-se com onboarding concluído
+            migrationBuilder.Sql(
+                "UPDATE Users SET IsOnboardingCompleted = 1 WHERE CompanyId IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOnboardingCompleted",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PositionId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
